acb87e4409ef43eba8d488ae7c627e76973d82d3
wzhua02 <woozongghua@gmail.com>
2025-06-24 15:57:08 +0800

    Chatbot template

--- src/main/java/com/cardproject/cardboard_cake/HomeController.java
+++ src/main/java/com/cardproject/cardboard_cake/HomeController.java
@@ -1,16 +1,40 @@
 package com.cardproject.cardboard_cake;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @Value("${spring.application.name}")
-    private String appName;
+    @GetMapping("/")
+    public String showChat(Model model) {
+        model.addAttribute("messages", new ArrayList<String>());
+        return "index";
+    }
 
-    @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    @PostMapping("/chat")
+    public String handleMessage(@RequestParam String userMessage, Model model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("You: " + userMessage);
+
+        // Simple bot logic
+        String botReply = getReply(userMessage);
+        messages.add("Bot: " + botReply);
+
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+    private String getReply(String message) {
+        switch (message.toLowerCase()) {
+        case "hello": return "Hi there!";
+        case "how are you?": return "I'm just a bot, but I'm fine!";
+        default: return "Sorry, I don't understand that.";
+        }
     }
 }
